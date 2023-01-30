4ec56b14ba0004a0aa832c02b1c2cffe15819c19
Rafid13iit <rafidssrr.6767@gmail.com>
2023-01-30 11:50:36 +0600

    New codes added

--- DIff_CDQ.cpp
+++ DIff_CDQ.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #define MAXN 10
 
 int main (void) {
@@ -9,11 +8,9 @@ int main (void) {
   double f[MAXN + 1]={ 1.0,1.04,1.16,1.36,1.64,2.0,2.44,2.96,3.56,4.24,5.0 };
   double df[MAXN + 1];
  
-  df[0] = ( f[1] - f[0] )/( x[1] - x[0] );  
-  for (i=1; i< MAXN; i++)
-    df[i] = ( f[i+1] - f[i-1] )/( x[i+1] - x[i-1] );  
-  df[i] = ( f[i] - f[i-1] )/( x[i] - x[i-1] );  
-
+  for (i=0; i<= MAXN; i++)
+    df[i] = ( f[i+1] - f[i-1] )/( x[i+1] - x[i-1] );
+ 
   printf("- - - - - - - - - - - - - - - - - - - -\n");
   printf("  i    x[i]       f[i]       df[i]\n");
   printf("- - - - - - - - - - - - - - - - - - - -\n");
---
+++ Diff_FDQ_2.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define f(x)   ( (x)*(x) + 1 )
+#define MAXN 10
+
+int main (void) {
+  int i;
+  double a = 0, b = 2, n = 10, h, x[MAXN + 1], y[MAXN + 1], df[MAXN + 1];
+
+  h = (b - a)/ n;
+  
+  x[0] = a; 
+  for (i=1; i<n; i++) x[i] = x[i-1] + h;
+  x[i] = b; 
+
+  for (i=0; i<=n; i++) y[i] = f(x[i]);
+
+  for (i=0; i<n; i++) df[i] = ( y[i+1] - y[i] )/( x[i+1] - x[i] );   
+
+  printf("- - - - - - - - - - - - - - - - - - - -\n");
+  printf("  i    x[i]       y[i]       df[i]\n");
+  printf("- - - - - - - - - - - - - - - - - - - -\n");
+
+  for (i=0; i<= n; i++)
+    printf("%3d %10lf %10lf %10lf\n",i,x[i],y[i],df[i] );
+    
+  printf("- - - - - - - - - - - - - - - - - - - -\n");
+
+   return EXIT_SUCCESS;
+}
