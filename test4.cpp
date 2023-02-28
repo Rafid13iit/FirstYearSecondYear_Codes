2daa3dd015be221bca1d8d1f8d3cc3bbfdbc8d81
Rafid13iit <rafidssrr.6767@gmail.com>
2023-02-28 23:34:30 +0600

    new files added

--- test4.cpp
+++ test4.cpp
@@ -13,11 +13,12 @@ int main()
     int starting_index = 0;
     char state_name;
 
-    for(int i = 0; i < num_states; i++)
-    {
+    for(int i = 0; i < num_states; i++){
            cin >> state_name;
+           
            state_names[starting_index] = state_name;
            state_indices[state_name] = starting_index;
+
            starting_index++;
     }
 
@@ -84,17 +85,97 @@ int main()
         cout << state_names[i] << " ---> " << epsilon_transitions[i] << endl;
     }
 
+
     map<int, map<int, string>> new_transition_matrix;
+
     int power = pow(2, num_states);
+
     string new_states_array[power];
+
     new_states_array[0] = epsilon_transitions[0];
+
     map<string, int> new_states_map;
+
     new_states_map[epsilon_transitions[0]] = 1;
+
     vector<char> each_state;
+
     int start_index = 0, end_index = 1;
+
 
     while(start_index < end_index)
     {
         for(int x = 0; x < num_transitions-1; x++)
         {
-            for(int i =
+            for(int i = 0; i < new_states_array[start_index].length(); i++)
+            {
+                if(transition_matrix[state_indices[new_states_array[start_index][i]]][x][0] != '0')
+                {
+                    for(int j = 0; j < num_states; j++) 
+                    {
+                        if(transition_matrix[state_indices[new_states_array[start_index][i]]][x][j] != '0')
+                        {
+                            each_state.push_back(transition_matrix[state_indices[new_states_array[start_index][i]]][x][j]);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+                  
+            for(int l = 0; l < each_state.size(); l++)
+            {
+                for(int k = 0; k < epsilon_transitions[state_indices[each_state[l]]].length(); k++){
+                    if(new_transition_matrix[start_index][x].find(epsilon_transitions[state_indices[each_state[l]]][k]) == string::npos)
+                    {
+                        new_transition_matrix[start_index][x]+=epsilon_transitions[state_indices[each_state[l]]][k];
+                    }
+                }
+            }
+                 
+            each_state.clear();
+
+            if(new_states_map[new_transition_matrix[start_index][x]] != 1)
+            {
+                new_states_array[end_index] = new_transition_matrix[start_index][x];
+                end_index++;
+            }
+               
+            new_states_map[new_transition_matrix[start_index][x]] = 1;
+        } 
+        
+        start_index++;
+     }
+     cout << "    DFA table" << endl;
+
+     for(int i = 0; i < new_states_map.size(); i++)
+     {
+         cout << new_states_array[i] << "\t" << " ---> " << "\t";
+
+         for(int j = 0; j < num_transitions - 1; j++)
+         {
+             cout << new_transition_matrix[i][j] << " |";
+         }
+
+         cout << endl;
+     }
+       
+    
+    return 0;
+}
+
+/*
+3 3
+1 2 3
+0 0 0
+2 0 0
+3 0 0
+2 3 0
+3 0 0
+0 0 0
+1 0 0
+0 0 0
+0 0 0
+*/
