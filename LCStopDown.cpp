27c3972b1e90c305ed4dbc33648ef1c801a35d13
Rafid13iit <rafidssrr.6767@gmail.com>
2023-03-02 07:30:05 +0600

    some files updated

--- LCStopDown.cpp
+++ LCStopDown.cpp
@@ -5,6 +5,24 @@ using namespace std;
 
 string str1, str2;
 
+void PrintLCS(int i, int j, string LCSstr, int LCSlen) {
+    if (str1[i] == '\0' || str2[j] == '\0') {
+
+        if (LCSstr.length() == LCSlen) {
+            cout << LCSstr << endl;
+        }
+        return ;
+    }
+
+    if (str1[i] == str2[j]) {
+        PrintLCS(i+1, j+1, LCSstr+str1[i], LCSlen);
+    } else {
+        PrintLCS(i+1, j, LCSstr, LCSlen);
+        PrintLCS(i, j+1, LCSstr, LCSlen);
+    }
+}
+
+
 int LCS(int i, int j)
 {
     if (str1[i] == '\0' || str2[j] == '\0'){
@@ -22,7 +40,7 @@ int LCS(int i, int j)
 
 int main ()
 {
-    freopen("text_LCS.txt", "r", stdin);
+    //freopen("text_LCS.txt", "r", stdin);
     
     int i = 0, j = 0;
 
@@ -31,5 +49,11 @@ int main ()
     cout << "Enter string 2 : ";
     cin >> str2;
 
-    cout << "Longest Common Subsequence length : " << LCS(i, j) << endl;
+    int LCSlen = LCS(i, j);
+
+    cout << "Longest Common Subsequence length : " << LCSlen << endl;
+    cout << "LCS is : ";
+    PrintLCS(i, j, "", LCSlen);
+
+    return 0;
 }
