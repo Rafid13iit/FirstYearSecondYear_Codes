ee64149f81f94bb8c371516ae20ee33ea7f787ae
Rafid <113895411+Rafid13iit@users.noreply.github.com>
2023-09-11 11:15:07 +0600

    Update Polyalphabetic_Encryp_Decryp.cpp

--- Polyalphabetic_Encryp_Decryp.cpp
+++ Polyalphabetic_Encryp_Decryp.cpp
@@ -3,52 +3,73 @@ using namespace std;
 
 string polyalphabetic_encrypt(string plaintext, string key) {
     string ciphertext = "";
-    
+
     for (int i = 0; i < plaintext.size(); i++) {
         char plainChar = plaintext[i];
         char keyChar = key[i % key.size()];
 
-        // Convert key character to 0-25 range
+        if (plainChar == ' ') {
+            ciphertext += ' ';
+            continue;
+        }
+
         keyChar = toupper(keyChar) - 'A';
 
-        // Encrypt the character
+        bool isUpper = isupper(plainChar);
+
+
         char cipherChar = (toupper(plainChar) - 'A' + keyChar) % 26 + 'A';
+
+        if (!isUpper) {
+            cipherChar = tolower(cipherChar);
+        }
+
         ciphertext += cipherChar;
     }
-    
+
     return ciphertext;
 }
 
 string polyalphabetic_decrypt(string ciphertext, string key) {
     string plaintext = "";
-    
+
     for (int i = 0; i < ciphertext.size(); i++) {
         char cipherChar = ciphertext[i];
         char keyChar = key[i % key.size()];
 
-        // Convert key character to 0-25 range
+        if (cipherChar == ' ') {
+            plaintext += ' ';
+            continue;
+        }
+
         keyChar = toupper(keyChar) - 'A';
 
-        // Decrypt the character
-        char plainChar = (toupper(cipherChar) - 'A' - keyChar + 26) % 26 + 'A';
+        bool isUpper = isupper(cipherChar);
+
+
+
+        if (!isUpper) {
+            plainChar = tolower(plainChar);
+        }
+
         plaintext += plainChar;
     }
-    
+
     return plaintext;
 }
 
 int main() {
     string plaintext, key;
-    
+
     cout << "Enter plaintext: ";
     getline(cin, plaintext);
-    
+
     cout << "Enter key: ";
     cin >> key;
 
     string encrypted = polyalphabetic_encrypt(plaintext, key);
     string decrypted = polyalphabetic_decrypt(encrypted, key);
-    
+
     cout << "Encrypted: " << encrypted << endl;
     cout << "Decrypted: " << decrypted << endl;
 
